70b4a64a0397efcbbb2c9219b57bd2041d7c91a4
slothagami <slothagami@gmail.com>
2024-05-20 22:35:03 +1000

    compare test and train accuracy

--- main.py
+++ main.py
@@ -11,12 +11,15 @@ mnist = tf.keras.datasets.mnist
 train_batch = train_batch.reshape(train_batch.shape[0], 28*28, 1).astype("float32") / 255 * 2 - 1
 test_batch  = test_batch .reshape(test_batch .shape[0], 28*28, 1).astype("float32") / 255 * 2 - 1
 
-# setup network
-nn = Network(MSE(), .005)
+# setup 
+lr = 0.04
+nn = Network(MSE(), lr)
 nn.config((28*28, 64, 10), FCLayer, Tanh)
 
 batch_size = 128
 epochs = 2
+
+print(f"Beginning training: {lr=}, {batch_size=}, {epochs=}\n")
 
 # train network
 error_plot = []
@@ -33,6 +36,18 @@ for epoch in range(epochs):
             print(f"Epoch {epoch+1} Error: {err:.8f}")
 
 
+
+# test accuracy
+correct = 0
+for sample, label in zip(train_batch, train_labels):
+    label = one_hot(10, label)
+    pred  = nn.forward(sample)
+
+    if np.argmax(pred) == np.argmax(label):
+        correct += 1
+
+print(f"\nTrain Accuracy: {correct}/{train_labels.size} ({correct/train_labels.size * 100:.2f}%)")
+
 # test accuracy
 correct = 0
 for sample, label in zip(test_batch, test_labels):
@@ -42,7 +57,7 @@ for sample, label in zip(test_batch, test_labels):
     if np.argmax(pred) == np.argmax(label):
         correct += 1
 
-print(f"Test Accuracy: {correct}/{test_labels.size} ({correct/test_labels.size * 100:.2f}%)")
+print(f"Test  Accuracy: {correct}/{test_labels.size} ({correct/test_labels.size * 100:.2f}%)\n")
 
 
 plt.plot(error_plot)
