89bc5d5073f270276166e4c3fd40c9b58885f701
slothagami <slothagami@gmail.com>
2025-08-29 18:35:45 +1000

    add docstrings to util functions

--- util.py
+++ util.py
@@ -1,18 +1,32 @@
 import numpy as np
 
-def one_hot(length, value):
+def one_hot(length: int, value: int):
+    """
+        Returns a one hot column vector with length `length` and value `value`.
+        For Example
+        ```
+        one_hot(3, 0)
+        > np.array([[1], 
+                    [0], 
+                    [0]])
+    """
     vector = np.zeros((length, 1))
     vector[value] = 1
     return vector
 
-def one_hot_accuracy(name, nn, samples, labels):
+def one_hot_accuracy(name: str, network, samples, labels, print=True):
+    """
+        Calculates the accuracy of the model over the data provided and optionally prints the percentage accuracy.
+    """
     correct = 0
     for sample, label in zip(samples, labels):
         label = one_hot(10, label)
-        pred  = nn.forward(sample)
+        pred  = network.forward(sample)
 
         if np.argmax(pred) == np.argmax(label):
             correct += 1
 
-    print(f"{name} Accuracy: {correct}/{labels.size} ({correct/labels.size * 100:.2f}%)")
+    if print:
+        print(f"{name} Accuracy: {correct}/{labels.size} ({correct/labels.size * 100:.2f}%)")
+
     return correct
