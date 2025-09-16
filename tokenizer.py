dd06500ad271c7364b488ef9af7f933d935551c0
slothagami <slothagami@gmail.com>
2025-09-17 03:16:06 +1000

    added simple word tokenizer

--- tokenizer.py
+++ tokenizer.py
@@ -1,6 +1,7 @@
 import string
+import re
 
-def make_tokens(dataset_text, n_tokens):
+def byte_pair_encoding(dataset_text, n_tokens):
     """Generates a byte pair encoding based on the text input with the specified number of tokens."""
 
     tokens  = list(string.printable) # initialize with one of every printable character
@@ -9,11 +10,22 @@ def make_tokens(dataset_text, n_tokens):
     while len(tokens) < n_tokens:
         # determine the most frequent token pair
         break
-    return tokens
+    return list(set(dataset))
+
+def remove_punctuation(text):
+    for symbol in string.punctuation:
+        text = text.replace(symbol, "")
+
+    return text
+
+def make_tokens(text):
+    text = remove_punctuation(text).lower()
+    words = re.split(r"\s+", text)
+    return list(set(words)) + [" ", "\n"]
 
 if __name__ == "__main__":
-    with open("datasets/rj.txt", "r") as file:
+    with open("datasets/rj.txt", "r", encoding="utf-8") as file:
         text = file.read()
 
-    tokens = make_tokens(text, 100)
-    print(tokens)
+    tokens = make_tokens(text)
+    print(tokens, "\n\n", len(tokens))
